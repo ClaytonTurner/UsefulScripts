4dd2670e1be792d97ad4f02c4fb65600fef4b574
Clayton Turner <learn2mine@gmail.com>
2015-11-04 14:05:00 -0500

    Adding Vundle clone to sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -6,6 +6,8 @@ apt-get install vim
 # Alias vi to vim for easy usage
 alias vi="vim"
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Let's mess with our vimrc now
 touch ~/.vimrc
 cat vimrc >> ~/.vimrc
